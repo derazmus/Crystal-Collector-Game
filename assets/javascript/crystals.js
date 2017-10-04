39c61bb5b6187f86ff78263a913661a281072edf
Denise Erazmus <denise.erazmus@gmail.com>
2017-10-03 22:46:48 -0500

    game reset function

--- assets/javascript/crystals.js
+++ assets/javascript/crystals.js
@@ -1,70 +1,87 @@
 $(document).ready(function(){
 
-//* generates random target number bewteen 19 and 120*//
-var targetNumber = Math.floor(12 + (Math.random() * (120 - 19)));
+//* generates random target number bewteen 120-12*//
+var targetNumber = Math.floor(12 + (Math.random() * (120 - 12)));;
+        $("#number-to-guess").text(targetNumber);
 
-$("#number-to-guess").text(targetNumber);
 
-//*variable needed  for tallies*//
-
+//* variables needed for tallies *//
 var wins = 0;
 var loses = 0;
 var counter = 0;
+var numberOptions= [];
 
+//* start function*//
+function start(){
+    counter= 0;   
+}
 
-//* create 4 random numbers bewtween 1 and 12 assigned to the crystals*//
- var numberOptions= [];
+//* creates crystals and numbers for crystals*//
 
- for(var i = 1; i <= 4; i++)
+function crystalCreate(){
+
+//* create 4 random numbers bewtween 1-12 assigned to the crystals*//
+
+ for(var i = 1; i <= 4; i++){
     numberOptions.push(Math.floor(12 + (Math.random() * (1 - 12))));
+    }  
+
 
 //*loop to create crystals for every number option*//
  for (var i=0; i<4; i++){
 
 //*for each interation we will create an imageCrystal
 //* need four different images*//
-
 var imageCrystal=$("<img>");
-
 imageCrystal.addClass("crystal-image");
-
-imageCrystal.attr("src","https://tse4.mm.bing.net/th?id=OIP.T-A3fgsXRXYp2Vur5ce6XADpEs&pid=15.1&P=0&w=300&h=300");
-
+imageCrystal.attr("src","http://www.batterycompany.co.uk/blog/wp-content/uploads/2016/11/diamond-06.jpg");
 imageCrystal.attr("data-crystalvalue", numberOptions[i]);
-
 
 $("#crystals").append(imageCrystal);
 }
+}
+
+//*function restart(){
+    //*counter=0;
+
+   //* var targetNumber = Math.floor(12 + (Math.random() * (120 - 12)));;
+        //*$("#number-to-guess").text(targetNumber);
+
+    //*for(var i = 1; i <= 4; i++){
+    //*numberOptions.push(Math.floor(12 + (Math.random() * (1 - 12))));
+   //* } 
+
+
+crystalCreate()
+start()
+
 //* when click on image get value*//
-//*need current tallie to show under  total score in the html*//
-
+//* put total in total score*//
 $(".crystal-image").on("click",function(){
-
     var crystalValue=($(this).attr("data-crystalvalue"));
     crystalValue=parseInt(crystalValue);
     counter+=crystalValue;
     $('#totalScore').text(counter);
 
-//* alerts  current score*//
-
-    alert("New score:"  + counter);
-
-//* when win*//
-
+    
+//* when win *//
     if (counter === targetNumber){
-        alert("You Win!");
         wins++;
         $('#wins').text(wins);
+        alert("You Win!");
+        //*restart();
+        start();
     }
 
-//* when lose*//
-
+//* when lose *//
     else if(counter>= targetNumber) {
-        alert("you lose");
-        loses++;
+         loses++;
         $('#loses').text(loses);
+        alert("you lose");
+        //*restart();
+        start();
     }
 
-//* need game reset*//
+//* game does restart but is not generating a new target number*//
 })
 });
